850333f5946a204017c2847d91eaadc9ca5b2f99
codebummer <holysmokes@hanmail.net>
2022-11-11 12:29:00 +0900

    Create ANN_test_with_multiplevariables.py

---
+++ ANN_test_with_multiplevariables.py
@@ -0,0 +1,176 @@
+#This program uses an artificial recurrent neural network called Long Short Term memory (LSTM)
+# to predict the closing stock price of a corporation (Apple Inc.) using the past 60 day stock price.
+
+#import the libraries
+import math
+import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+from datetime import datetime
+plt.style.use('fivethirtyeight')
+
+
+start = datetime(2022, 1, 1)
+end = datetime(2022, 11, 11)
+
+currencies = {
+    'KRW' : 'KRW=X', 
+    'CNY' : 'CNY=X',
+    'JPY' : 'JPY=X',
+    'EUR' : 'EUR=X',
+    'GBP' : 'GBP=X',
+    'CHF' : 'CHF=X',
+    'CAD' : 'CAD=X',
+    'SEK' : 'SEK=X'
+    }
+
+stocks = ['SAMSUNG','005930.KS']
+stock_factors = ['Volume', 'Adj Close']
+
+df = pd.DataFrame([])
+for currency, ticker in currencies.items():
+    df[currency] = web.DataReader(ticker, 'yahoo', start, end)['Adj Close']
+
+for factor in stock_factors:
+    df[factor] = web.DataReader(stocks[1], 'yahoo', start, end)[factor]
+
+df.dropna()
+scaled = pd.DataFrame([])
+scaler = MinMaxScaler(feature_range = (0, 1))
+for col in df.columns:
+    scaled[col] = scaler.fit_transform(df[col].values.reshape(-1, 1))
+scaled.dropna()
+df.dropna()
+scaled_data = scaled.stack()
+df = df.stack()
+
+
+# #Convert the dataframe to a numpy array
+dataset = df.values
+#Get the number of rows to train the model on
+training_data_len = math.ceil(len(dataset) * 0.8)
+
+#Scale the data
+# scaler = MinMaxScaler(feature_range = (0, 1))
+# scaled_data = scaler.fit_transform(dataset)
+
+#Create the training data set
+#Create the scaled training data set
+# train_data = scaled_data[0 : training_data_len, :]
+train_data = scaled_data[0 : training_data_len, :]
+
+#Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60 : i, 0])
+    y_train.append(train_data[i, 0])
+    if i <= 61:
+        print(x_train)
+        print(y_train)
+        print()
+
+#Convert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+#Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# x_train.shape[0]: number of samples(rows)
+# x_train.shape[1]: number of time stamps(columns)
+# 1: number of features. ANN takes three dimentional values as inputs
+
+x_train.shape
+
+
+#Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences = True, input_shape = (x_train.shape[1], 1))) #adding a LSTM layer (the first layer) to the model
+model.add(LSTM(50, return_sequences = False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+#Compile the model
+model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+#Train the model
+model.fit(x_train, y_train, batch_size = 1, epochs = 1)
+
+#Create the testing data set
+#Create a new array containing scaled values from index 1543 to 2003
+test_data = scaled_data[training_data_len - 60 : , :]
+#Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len : , :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+#Convert the data to a numpy array
+x_test = np.array(x_test)
+
+#Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+#Get the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions) #Inverse transform data
+
+#Get the root mean squared error(RMSE)
+rmse = np.sqrt(np.mean(predictions - y_test) ** 2)
+
+#Plot th data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+
+#Visualize the data
+plt.figure(figsize = (16, 8))
+plt.title('Model')
+plt.xlabel('Data', fontsize = 18)
+plt.ylabel('Close Price USD ($)', fontsize = 18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc = 'lower right')
+plt.show()
+
+#Calculate a predicted price for the next day
+print(f"Price Predicted on {data.index[-1]}: ", scaled_data[-1] * predictions[-1] / predictions_scaled[-1])
+#print(f"Price Predicted on {data.index[-1]}: ", dataset[-1] / x_test[-1,-1,0] * predictions_scaled[-1])
+
+
+
+
+
+
+#Show the valid and predicted prices
+print(valid)
+
+#Get the quote
+apple_quote = web.DataReader('AAPL', data_source = 'yahoo', start = '2012-01-01', end = '2019-12-17')
+#Create a new dataframe
+new_df = apple_quote.filter(['Close'])
+#Get the last 60 day closing price values and convert the dataframe to an array
+last_60_days = new_df[-60:].values
+#Scale the data to be values between 0 and 1
+last_60_days_scaled = scaler.transform(last_60_days)
+#Create an empty list
+X_test = []
+#Append the past 60 days
+X_test.append(last_60_days_scaled)
+#Convert the X_test data set to a numpy array
+X_test = np.array(X_test)
+#Reshape the data
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#Get the predicted scaled price
+pred_price = model.predict(X_test)
+#Undo the scaling
+pred_price = scaler.inverse_transform(pred_price)
+print(pred_price)
+
+#Get the quote
+apple_quote2 = web.DataReader('AAPL', data_source = 'yahoo', start = '2019-12-18', end = '2019-12-18')
+print(apple_quote2['Close'])
