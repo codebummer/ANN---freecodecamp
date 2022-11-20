d59eeb6b106127fcbd5896830121becce658ab74
codebummer <holysmokes@hanmail.net>
2022-11-20 10:51:14 +0900

    Update coloray_RNN.py

--- coloray_RNN.py
+++ coloray_RNN.py
@@ -6,7 +6,7 @@ import math
 import pandas_datareader as web
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler #install scikit-learn to import sklearn
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
